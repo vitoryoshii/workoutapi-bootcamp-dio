c3bd33ba774469f84c65ccc9b72b57cf2f1c71dc
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-07-26 13:16:15 -0300

    fecth: colocado endpoints delete e patch

--- workoutapi/app/atleta/schemas.py
+++ workoutapi/app/atleta/schemas.py
@@ -22,8 +22,11 @@ class AtletaIn(Atleta):
     pass
 
 
-class AtletaOut(Atleta, OutMixin):
-    pass
+class AtletaOut(OutMixin):
+    nome: Annotated[str, Field(description='Nome do atleta', example='João Ferreira Silva', max_length=50)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
+    
 
 
 class AtletaUpdate(BaseSchema):
